b8d9b2d29c039ebf1582ff657d02f3ef1d5fedae
포도맛 버섯 <skacjstk0818@gmail.com>
2020-03-01 16:30:46 +0900

    [2020.03.01.] 파일 분할

---
+++ HelloWorld2/HelloWorld2/Sort.h
@@ -0,0 +1,16 @@
+#pragma once 
+#include <iostream>
+#include <cstdlib>		//랜덤 함수 생성 용
+#include <ctime>	//시간 시드용
+
+
+#define MAX_ARRAY 10
+
+extern int array[MAX_ARRAY];
+
+void dataShow(const int *data);
+void quickSort(int *data, int start, int end);
+
+void dataShow(const int *data);
+void randomInputData(int *data);
+void randomInputData(int *data, int start, int end);
