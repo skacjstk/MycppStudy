ac6164c59dbb2aad17a64c608277512db1279ef0
포도맛 버섯 <skacjstk0818@gmail.com>
2020-03-19 20:50:36 +0900

    [2020.03.19] 피보나치+시간측정

--- HelloWorld2/dataStructure/Fibonacci.cpp
+++ HelloWorld2/dataStructure/Fibonacci.cpp
@@ -2,17 +2,16 @@
 #include <vector>
 #include <chrono>
 int fibonacci(std::vector<int> &memo,const int x) {
-	if (x == 1) return 1;
-	if (x == 2) return 1;
+	if (x <= 2) return 1;
 	if (memo[x]) return memo[x];
 	else
 		return memo[x] = fibonacci(memo, x - 1) + fibonacci(memo, x - 2);
 }
 int fibonacci(std::vector<int> &memo, const int x, bool flag){
-	if (x == 1) return 1;
-	if (x == 2) return 1;
+		if (x <= 2) return 1;
 	else
-		return memo[x] = fibonacci(memo, x - 1) + fibonacci(memo, x - 2);
+		return fibonacci(memo, x - 1,false) + fibonacci(memo, x - 2,false);
+		//깜빡하고 여기에 아무 bool flag 를 안넣어서 두번쨰 함수부터 메모이제이션을 해버렸다.
 }
 
 int main(void)
@@ -22,17 +21,18 @@ int main(void)
 	std::cin >> num;
 	std::vector<int> memo(num+1);
 
-//	auto start = std::chrono::steady_clock::now();
+	auto start = std::chrono::steady_clock::now();
 	printf("%d\n", fibonacci(memo, num));
-//	auto fiboEnd = std::chrono::steady_clock::now();
-//	printf("%d\n", fibonacci(memo, num, false));
-//	auto noMemoiFiboEnd = std::chrono::steady_clock::now();
+	auto fiboEnd = std::chrono::steady_clock::now();
+	std::cout << "피보나치 수열(메모이제이션) 소모 시간 : "
+		<< std::chrono::duration_cast<std::chrono::microseconds>(fiboEnd - start).count() << " us" << "\n";
 
-//	std::cout << "피보나치 수열(메모이제이션) 소모 시간 : "
-//		<< std::chrono::duration_cast<std::chrono::milliseconds>(fiboEnd - start).count()<< " ms" << "\n";
-//	std::cout << "피보나치 수열(no메모이제이) 소모 시간 : "
-//		<< std::chrono::duration_cast<std::chrono::milliseconds>(noMemoiFiboEnd - fiboEnd).count() << " ms" << "\n";
+	auto noMemoStart = std::chrono::steady_clock::now();	//정확한 측정을 위해
+	printf("%d\n", fibonacci(memo, num, false));
+	auto noMemoiFiboEnd = std::chrono::steady_clock::now();	
+	std::cout << "피보나치 수열(no메모이제이) 소모 시간 : "
+		<< std::chrono::duration_cast<std::chrono::microseconds>(noMemoiFiboEnd - noMemoStart).count() << " us" << "\n";
 
-	//chrono 는 이렇게 쓰는게 아닌가보다. 
+	//chrono 시간측정이 재대로 안된다.
 	return 0;
 }
