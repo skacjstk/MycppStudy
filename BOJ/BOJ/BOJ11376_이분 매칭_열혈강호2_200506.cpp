7d1c8125a55035b059f2f5d5b2f5625c3db4df93
포도맛 버섯 <skacjstk0818@gmail.com>
2020-05-15 20:52:29 +0900

    [2020.05.06] 백준 11376번 이분 매칭 열혈강호2

--- BOJ/BOJ/BOJ11376_이분 매칭_열혈강호2_200506.cpp
+++ BOJ/BOJ/BOJ11376_이분 매칭_열혈강호2_200506.cpp
@@ -3,6 +3,75 @@
 #include <vector>
 
 #define MAX 1000 
+
+/*
+typedef struct _sharkData {
+	int Ssize;
+	int	speed;
+	int	intel;
+	
+	_sharkData(int _size, int _speed, int _intel) :
+	Ssize(_size), speed(_speed), intel(_intel){}
+
+}sharkData;
+*/
+
+//std::vector<std::vector<sharkData>> node;	//원소가 담기는 곳
+
+int sharkNode[MAX][3];
+std::vector<bool> visited;	//방문검사
+int b[MAX +1]; //i번째 작업과 매칭된 b[i]번째 사람.
+std::vector<int> node[MAX];
+
+bool dfs(int here) {
+	for (int there : node[here]) { .//고정배열이니까 이렇게하면 안됌
+		if (visited[there]) //return 0;
+			continue;
+
+		visited[there] = true;
+		if (!b[there] || dfs(b[there])) {
+			b[there] = here;
+			return true;
+		}
+	}
+
+	return false;
+
+}
+
+int bmatch(const int sharkNum) {
+	int ret = sharkNum;
+
+	
+
+
+	return ret;
+}
+
+void testCase() {
+	int sharkNum;
+	scanf("%d", &sharkNum);
+	//상어 메모리 초기화  
+	for (int i = 1; i <= sharkNum; ++i) {
+		scanf("%d %d %d", &sharkNode[i][0], &sharkNode[i][1], &sharkNode[i][2]); //상어 크기 속도 지능
+	}
+	//데이터 입력 완료
+	printf("%d", bmatch(sharkNum));
+}
+
+int main(void)
+{
+	testCase();
+	return 0;
+
+}
+
+/*
+#define _CRT_SECURE_NO_WARNINGS
+#include <iostream>
+#include <vector>
+
+#define MAX 1000
 std::vector<std::vector<int>> node;	//원소가 담기는 곳
 std::vector<bool> visited;	//방문검사
 int b[MAX +1]; //i번째 작업과 매칭된 b[i]번째 사람.
@@ -61,3 +130,4 @@ int main(void)
 	return 0;
 
 }
+*/
