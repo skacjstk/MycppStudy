26b3234fda3cd8c0c49ab1237a1d1eaeb5025fb5
포도맛 버섯 <skacjstk0818@gmail.com>
2020-06-18 22:18:34 +0900

    [2020.06.17] weak_ptr

--- HelloWorld2/dataStructure/week_ptr_200617.cpp
+++ HelloWorld2/dataStructure/week_ptr_200617.cpp
@@ -23,11 +23,34 @@ public:
 		otherPtr = op;
 	}
 	void checkPtr() {
-
+		std::shared_ptr<TreesNode> o = otherPtr.lock();
+		if (o) {
+			std::cout << "존재함 : " << o->getName() <<"\n";
+		}
+		else {
+			std::cout << "이미 소멸되었다" << "\n";
+		}
 	}
 };
 int main(void) {
+	std::vector<std::shared_ptr<TreesNode>> vec1;
+	vec1.push_back(std::make_shared<TreesNode>("0번째"));
+	vec1.push_back(std::make_shared<TreesNode>("1번째"));
 
+	vec1[0]->setOtherPtr(vec1[1]);
+	vec1[1]->setOtherPtr(vec1[0]);	//서로의 weakPtr 연결
+
+	// 참조 개수는 그대로
+	std::cout << "vec1[0] "<<vec1[0]->getName() <<" 참조 개수 : " << vec1[0].use_count() << "\n";
+	std::cout << "vec1[1] "<< vec1[1]->getName() <<" 참조 개수 : " << vec1[1].use_count() << "\n";
+
+	// weak_ptr 로 해당 객체 접근하기
+	vec1[0]->checkPtr(); 
+	vec1[1]->checkPtr();
+
+	// 벡터 마지막 원소 제거 (vec[1] 소멸)
+	vec1.pop_back();
+	vec1[0]->checkPtr(); //vec1[1] 객체는 이미 소멸되었다. 
 
 	return 0;
 }
