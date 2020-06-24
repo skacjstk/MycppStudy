e868190634ee8c97e9211251b53600caf358576a
포도맛 버섯 <skacjstk0818@gmail.com>
2020-06-24 21:34:02 +0900

    [2020.06.23] Lambda

--- HelloWorld2/dataStructure/Lambda_200623.cpp
+++ HelloWorld2/dataStructure/Lambda_200623.cpp
@@ -2,6 +2,14 @@
 #include <vector>
 #include <algorithm>
 using namespace std;
+template <typename T>
+void fill(vector<int>& v, T done) {
+	int i = 0;
+	while (!done()) {
+		v.push_back(i++);
+	}
+}
+
 int main(void)
 {
 	int total_elements = 1;
@@ -13,11 +21,16 @@ int main(void)
 	cardinal.push_back(4);
 	cardinal.push_back(8);
 
-	for_each(cardinal.begin(), cardinal.end(), [&](int i) {std::cout << i << testEl<<"\n"; total_elements *= i; });
-
-	//람다의 캡처절 [] 은 변수명 지정 안하면 전범위 접근이고 가져오면 const 붙고 
-	//대충 많음 (미완성)
-
+	for_each(cardinal.begin(), cardinal.end(), [&](int i)
+	{std::cout << i << testEl<<"\n"; total_elements *= i; });
 	cout << "total elements : " << total_elements << endl;
+
+	//람다의 캡처절 [] 은 변수명 지정 안하면 개체범위 접근이고 가져오면 const 붙고 
+
+	vector<int> stuff;
+	fill(stuff, [&stuff]() -> bool { return stuff.size() >= 8; });
+
+
+	for_each(stuff.begin(), stuff.end(), [](int i) { cout << i << " "; });
 	return 0;
 }
