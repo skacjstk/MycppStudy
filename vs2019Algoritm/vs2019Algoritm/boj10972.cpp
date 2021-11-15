463b73a76a58e789b3975099c52a3d7b986f2a9c
포도맛 버섯 <skacjstk0818@gmail.com>
2021-11-15 17:33:38 +0900

    [2021.11.15]  boj10972 다음 순열(라이브러리 함수 사용)

---
+++ vs2019Algoritm/vs2019Algoritm/boj10972.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+#include <vector>
+#include <algorithm>
+using namespace std;
+
+int n, m;
+//int arr[MAX] = { 0, };
+//bool visited[MAX] = { 0, };
+vector<int> number; //인덱스가 담긴 N 들
+
+
+int main() {
+    cin >> n;
+    number.resize(n);
+
+    for (int i = 0; i < n; ++i) {
+        cin >> number[i];
+    }
+
+    if (next_permutation(number.begin(), number.end())) {
+        for (int i = 0; i < n; ++i) {
+            cout << number[i] << " ";
+        }
+    }
+    else
+        cout << "-1";
+
+    return 0;
+}
+//다음 순열을 구하는 C++ 함수 라이브러리가 있음
+//벡터에 다음 순열이 직접 적용된다.
+//반환값은 true, false 이다. 
