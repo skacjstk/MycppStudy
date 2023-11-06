63600f3f1364d8487f178c296bbdee995c6faa41
포도맛 버섯 <skacjstk0818@gmail.com>
2023-11-06 17:16:31 +0900

    [2023.11.06] 백준 1912 연속합 DP

--- BOJ/BOJ/BOJ_1912_연속합.cpp
+++ BOJ/BOJ/BOJ_1912_연속합.cpp
@@ -2,31 +2,39 @@
 #include <vector>
 #include <algorithm>
 #include <numeric>
-
 using namespace std;
 
+int OldSolve(vector<int>& Arr)
+{
+	int Result = INT32_MIN;
+	int Count = Arr.size();
+
+	for (int i = 0; i < Count; ++i)
+	{
+		for (int j = Count - 1; j >= i; --j)
+		{
+			int Sum = accumulate(&Arr[i], &Arr[j] + 1, 0);
+			Result = max(Sum, Result);
+		}
+	}
+	return Result;
+}
 int Solve(vector<int>& Arr)
 {
 	vector<int> Sums;	// 0 부터 index 까지의 합
 	int Result = INT32_MIN;
 	int Count = Arr.size();
-	Sums = Arr;
 
+	Sums = Arr;
 	Result = Sums[0];
 	for (int i = 1; i < Count; ++i)
 	{
-
-		if (Sums[(size_t)i -1] < 0)		
-			Sums[i] = Arr[i];		
-		else
-			Sums[i] = Sums[i] + Sums[(size_t)i - 1];
-
+		Sums[i] = max(Sums[i] + Sums[(size_t)i - 1], Sums[i]); // 새로 시작하기 vs 이전 값 합치기 n-1 + n vs n 
 		Result = max(Sums[i], Result);
 	}
 	return Result;
 }
-
-int main(void)
+int main(void) 
 {
 	int N;
 	vector<int> Arr;
@@ -36,7 +44,6 @@ int main(void)
 	{
 		cin >> Arr[i];
 	}
-
 	cout << Solve(Arr);
 	return 0;
 }
