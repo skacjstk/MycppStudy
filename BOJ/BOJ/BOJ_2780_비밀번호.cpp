aec9b50111db1579c9c4b723a14436a1a8608eb2
포도맛 버섯 <skacjstk0818@gmail.com>
2023-11-09 20:29:12 +0900

    [2023.11.09] 백준 2780번 비밀번호 DP

--- BOJ/BOJ/BOJ_2780_비밀번호.cpp
+++ BOJ/BOJ/BOJ_2780_비밀번호.cpp
@@ -1,38 +1,82 @@
 #include <iostream>
 #include <vector>
-
+// DP
 #define DIVNUM 1234567
 
 using namespace std;
-vector < vector<int>> DP;
+vector < vector<int>> dp;
 
-int NextPassword(int Depth, int current)
+int NextPassword(int N, int current)
 {
-	if (Depth == 1) return 1;
-	if (DP[Depth][current] != -1) 
-		return DP[Depth][current];
-
+	if (N == 1)return 1;
+	if (dp[N][current] != -1)return dp[N][current];
 	
+	int result;
+
+    switch (current)
+    {
+    case 0:
+        result = NextPassword(N - 1, 7) % DIVNUM;
+        dp[N][current] = result;
+        return result;
+    case 1:
+        result = NextPassword(N - 1, 2) % DIVNUM + NextPassword(N - 1, 4) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 2:
+        result = NextPassword(N - 1, 1) % DIVNUM + NextPassword(N - 1, 3) % DIVNUM + NextPassword(N - 1, 5) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 3:
+        result = NextPassword(N - 1, 2) % DIVNUM + NextPassword(N - 1, 6) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 4:
+        result = NextPassword(N - 1, 1) % DIVNUM + NextPassword(N - 1, 5) % DIVNUM + NextPassword(N - 1, 7) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 5:
+        result = NextPassword(N - 1, 2) % DIVNUM + NextPassword(N - 1, 4) % DIVNUM + NextPassword(N - 1, 6) % DIVNUM + NextPassword(N - 1, 8) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 6:
+        result = NextPassword(N - 1, 3) % DIVNUM + NextPassword(N - 1, 5) % DIVNUM + NextPassword(N - 1, 9) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 7:
+        result = NextPassword(N - 1, 4) % DIVNUM + NextPassword(N - 1, 8) % DIVNUM + NextPassword(N - 1, 0) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 8:
+        result = NextPassword(N - 1, 5) % DIVNUM + NextPassword(N - 1, 7) % DIVNUM + NextPassword(N - 1, 9) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    case 9:
+        result = NextPassword(N - 1, 6) % DIVNUM + NextPassword(N - 1, 8) % DIVNUM;
+        dp[N][current] = result % DIVNUM;
+        return dp[N][current];
+    }
 }
 
-int Solve(int N, int T)
+int Solve(int N, int i)
 {
-	int Result = 0;
-	DP.resize(T, std::vector<int>(10, 1));
-
-
-	return Result;
+	return NextPassword(N, i);
 }
 int main(void)
 {
 	int T, N;
-
 	cin >> T;
-	for (int i = 0; i < T; ++i)
-	{
-		cin >> N;
-		cout << Solve(N, T) % 1234567;
-	}	
+    dp.resize(1001, std::vector<int>(10, -1));
+
+    for (int k = 0; k < T; ++k)
+    {
+        cin >> N;
+        int Result = 0;
+        for (int i = 0; i < 10; i++) {
+            Result = (Result % DIVNUM + Solve(N, i) % DIVNUM) % DIVNUM;
+        }
+        cout << Result << '\n';
+    }
 
 	return 0;
 }
